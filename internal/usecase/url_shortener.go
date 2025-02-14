0a17e0287103bf0eaf0499bebb84518e2107b749
qosmioo <79136983+qosmioo@users.noreply.github.com>
2025-02-14 04:16:28 +0300

    added: service layer

---
+++ internal/usecase/url_shortener.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"github.com/qosmioo/ulr-shortener/internal/storage"
+	"github.com/qosmioo/ulr-shortener/pkg/shortener"
+	"github.com/qosmioo/ulr-shortener/pkg/validator"
+)
+
+type URLShortenerService struct {
+	store storage.Storage
+}
+
+func NewURLShortenerService(store storage.Storage) *URLShortenerService {
+	return &URLShortenerService{store: store}
+}
+
+func (u *URLShortenerService) CreateShortURL(originalURL string) (string, error) {
+	existingShortURL, err := u.store.GetShortURLByOriginal(originalURL)
+	if err == nil {
+		return existingShortURL, storage.ErrURLExists
+	}
+
+	shortURL := shortener.GenerateShortURL()
+
+	if err := validator.ValidateShortURL(shortURL); err != nil {
+		return "", err
+	}
+
+	if err := u.store.SaveURL(shortURL, originalURL); err != nil {
+		return "", err
+	}
+	return shortURL, nil
+}
+
+func (u *URLShortenerService) GetOriginalURL(shortURL string) (string, error) {
+	return u.store.GetURL(shortURL)
+}
